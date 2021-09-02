0d5a57c368f9326176f212ef0e483b49b512aaa4
Almonacid98 <Almonacid98>
2021-09-02 03:39:34 -0300

    Se agregaron importaciones requeridas

--- Bookmaker-DS/main/map/__init__.py
+++ Bookmaker-DS/main/map/__init__.py
@@ -1,1 +1,11 @@
+#Schema importación
 from .cliente_schema import ClienteSchema
+from .empresa_schema import EmpresaSchema
+from .equipo_schema import EquipoSchema
+from .apuesta_schema import ApuestaSchema
+from .partido_schema import PartidoSchema
+from .cuota_schema import CuotaSchema
+
+
+#Filtros importación
+from .cliente_filtros import ClienteFiltros
