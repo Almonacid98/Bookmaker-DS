87ae90f76ebe3f52041cce047a5c3f494a47dc6e
Almonacid98 <Almonacid98>
2021-09-02 03:40:33 -0300

    Se agregaron importaciones requeridas

--- Bookmaker-DS/main/models/__init__.py
+++ Bookmaker-DS/main/models/__init__.py
@@ -1,2 +1,6 @@
 from .cliente import Cliente as ClienteModel
 from .empresa import Empresa as EmpresaModel
+from .equipo import Equipo as EquipoModel
+from .apuesta import Apuesta as ApuestaModel
+from .partido import Partido as PartidoModel
+from .cuota import Cuota as CuotaModel
