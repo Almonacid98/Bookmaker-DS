75542f1ec9a3d9c92c79c99c02a454332666e735
Almonacid98 <Almonacid98>
2021-09-02 16:54:34 -0300

    Se agregaron los mapeo de apuesta, partido y cuota 'Schema'

---
+++ Bookmaker-DS/main/map/apuesta_schema.py
@@ -0,0 +1,15 @@
+from typing_extensions import Required
+from marshmallow import Schema, fields, validate
+from marshmallow.decorators import post_load
+from main.models import ApuestaModel
+
+class ApuestaSchema(Schema):
+
+    id = fields.Int(dump_only = True)
+    fecha = fields.DateTime(required = True)
+    monto = fields.Float(required = True)
+    equipo_ganador = fields.Int(required = True)
+
+    @post_load
+    def make_apuesta(self, data, **kwargs):
+        return ApuestaModel(**data)
---
+++ Bookmaker-DS/main/map/cuota_schema.py
@@ -0,0 +1,15 @@
+from typing_extensions import Required
+from marshmallow import Schema, fields, validate
+from marshmallow.decorators import post_load
+from main.models import CuotaModel
+
+class CuotaSchema(Schema):
+
+    id = fields.Int(dump_only = True)
+    probabilidad_local = fields.Float(required = True)
+    probabilidad_empate = fields.Float(required = True)
+    probabilidad_visitante = fields.Float(required = True)
+
+    @post_load
+    def make_cuota(self, data, **kwargs):
+        return CuotaModel(**data)
