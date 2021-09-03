6666a915f4f9dee30f50b1fa5bd076e34c9cffb6
Almonacid98 <Almonacid98>
2021-09-03 17:58:15 -0300

    Se agregaron lectores de csv para datos

--- Bookmaker-DS/app.py
+++ Bookmaker-DS/app.py
@@ -1,9 +1,59 @@
 from main import create_app
 import os
+from main import db
+from faker import Faker
+from main.models import ClienteModel, EquipoModel, CuotaModel, PartidoModel
+import csv
+from datetime import datetime
 app = create_app()
 app.app_context().push()
-from main import db
+
+def load_clientes():
+    fake = Faker('es_ES')
+    for _ in range(100):
+        cliente = ClienteModel(nombre=fake.first_name(), apellido=fake.name(), email=fake.email())
+        db.session.add(cliente)
+        db.session.commit()
+    
+    db.session.close()
+   
+    
+
+def load_equipos():
+    with open('./docs/equipo.csv', encoding='utf-8') as csv_file:
+        try:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                equipo = EquipoModel(nombre=row[0], escudo=row[1], pais=row[2], puntaje=float(row[3]))
+                
+                
+                equipo.cuota = cuota
+                db.session.add(equipo)
+
+                db.session.commit()
+            db.session.close()
+        except:
+            db.session.rollback()
+
+
+
+def load_partidos():
+    formato = "%d/%m/%Y %H:%M"
+    with open('./docs/partidos.csv', encoding='utf-8') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                partido = PartidoModel(fecha = row[0], equipo_local = row[1], equipo_visitante = row[2], finalizado = False, goles_local = 0, goles_visitante = 0)
+                cuota = CuotaModel(probabilidad_local=float(row[3]),probabilidad_empate=float(row[4]), probabilidad_visitante=float(row[5]))
+                partido.cuota = cuota
+                db.session.add(partido)
+                db.session.commit()
+            db.session.close()    
+
+
 
 if __name__ == '__main__':
+    db.drop_all()
     db.create_all()
+    load_equipos()
+    load_clientes()
     app.run(debug=True, port=os.getenv("PORT"))
